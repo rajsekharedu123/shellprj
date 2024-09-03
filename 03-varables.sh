8129dfaf2eb44c867073d6d563df79e3aa405612
rajsekhar <rajsekhar9655@gmail.com>
2024-09-03 22:26:37 +0530

    03 new prg

---
+++ 03-varables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1 :: hi how ru "
+echo "${PERSON2} :: i am fine $PERSON1, how abt u "
+echo "$PERSON1 :: what are u doing ${PERSON2} "
+echo "$PERSON1 :: learning devops"
+echo "${PERSON2} :: ok good , $PERSON1 "
