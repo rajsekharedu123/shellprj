8f12bb905a2ef35f9c257fa86eabbc81dc4dc97e
rajsekhar <rajsekhar9655@gmail.com>
2024-09-04 14:50:37 +0530

    changes

--- 10-conditiontesh.sh
+++ 10-conditiontesh.sh
@@ -4,7 +4,7 @@ number=$1
 
 if [ $number -gt 50 ]
 then 
-    echo " number entered is greated the 50 $number "
+    echo " number entered is greated the 50 \n $number "
 else 
  
     echo " number entered is less the 50 $number \n"
