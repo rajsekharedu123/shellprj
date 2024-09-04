d2db9a06cf98335dfd58d657a8ea7d6dd4d8b783
rajsekhar <rajsekhar9655@gmail.com>
2024-09-04 14:45:22 +0530

    changes

--- 10-conditiontesh.sh
+++ 10-conditiontesh.sh
@@ -1,13 +1,11 @@
 #!bin/bash
 
-number=$runvar
+number=$1
 
 if [ $number -gt 50 ]
 then 
-
     echo " number entered is greated the 50 $number "
 else 
  
     echo " number entered is less the 50 $number "
-
 fi 
