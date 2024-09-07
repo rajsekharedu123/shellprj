ad15d03193580f3579c1f134b53128c740068d5c
rajsekhar <rajsekhar9655@gmail.com>
2024-09-07 22:43:55 +0530

    changes

--- 001test.sh
+++ 001test.sh
@@ -7,5 +7,18 @@ echo $SCRIPT_NAME
 echo $TIMESTAMP
 echo 
 
+CHECK_ROOT(){
+
+    if [$USERID -ne 0]
+    then 
+        echo " pls use root access/privilages" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo "u logged with user : $USERID " | tee -a $LOG_FILE
+    fi
+}
+
 
 echo -e "$R script started executing  at $TIMESTAMP $N" | tee -a $LOG_FILE
+USERID=$(id -u)
+CHECK_ROOT
