c4793d196de5fbe8ed6da6344af9e568e10769e6
rajsekhar <rajsekhar9655@gmail.com>
2024-09-07 21:51:17 +0530

    changes

--- 001test.sh
+++ 001test.sh
@@ -3,3 +3,8 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
+echo $SCRIPT_NAME
+echo $TIMESTAMP
+echo 
+
+echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
